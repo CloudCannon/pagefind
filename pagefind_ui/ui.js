4bb9d8c687b1d0649c290b155498bf3b51bf7610
Liam Bigelow <40188355+bglw@users.noreply.github.com>
2022-07-01 16:36:35 +1200

    Fix PagefindUI Bundle Path

--- pagefind_ui/ui.js
+++ pagefind_ui/ui.js
@@ -1,21 +1,20 @@
 import PagefindSvelte from './svelte/ui.svelte';
 
+let scriptBundlePath;
+try {
+    scriptBundlePath = new URL(document.currentScript.src).pathname.match(/^(.*\/)(?:pagefind-)?ui.js.*$/)[1];
+} catch (e) {
+    scriptBundlePath = "/_pagefind/";
+    console.warn(`Pagefind couldn't determine the base of the bundle from the javascript import path. Falling back to the default of ${bundlePath}.`);
+    console.warn("You can configure this by passing a bundlePath option to PagefindUI");
+    console.warn(`[DEBUG: Loaded from ${document.currentScript.src ?? "unknown"}]`);
+}
+
 class PagefindUI {
     constructor(opts) {
         let selector = opts.element ?? "[data-pagefind-ui]";
-        let bundlePath = opts.bundlePath;
+        let bundlePath = opts.bundlePath ?? scriptBundlePath;
         let resetStyles = opts.resetStyles ?? true;
-
-        if (!bundlePath) {
-            try {
-                bundlePath = new URL(document.currentScript.src).pathname.match(/^(.*\/)(?:pagefind-)?ui.js.*$/)[1];
-            } catch (e) {
-                bundlePath = "/_pagefind/";
-                console.warn(`Pagefind couldn't determine the base of the bundle from the javascript import path. Falling back to the default of ${bundlePath}.`);
-                console.warn("You can configure this by passing a bundlePath option to PagefindUI");
-                console.warn(`[DEBUG: Loaded from ${document.currentScript.src ?? "unknown"}]`);
-            }
-        }
 
         // Remove the UI-specific config before passing it along to the Pagefind backend
         delete opts["element"];
