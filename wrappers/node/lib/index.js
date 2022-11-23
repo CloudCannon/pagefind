400ad409baed488fca34d03cecd20ff24ec5f1f8
Liam Bigelow <40188355+bglw@users.noreply.github.com>
2022-11-23 13:57:33 +1300

    Update node wrapper to pass error codes through from the Rust binary

--- wrappers/node/lib/index.js
+++ wrappers/node/lib/index.js
@@ -1,15 +1,26 @@
 #!/usr/bin/env node
 const path = require('path');
+const fs = require('fs');
 const { execFileSync } = require('child_process');
+
+const binaryPath = path.join(__dirname, `../bin/pagefind_extended${process.platform === 'win32' ? '.exe' : ''}`);
+
+if (!fs.existsSync(binaryPath)) {
+    console.error(`Binary failed to download — package expected to have downloaded the binary to ${binaryPath} during its postinstall`);
+    console.error(`Do you have install scripts disabled for npm?`);
+    console.error(`Try running \`npm config set ignore-scripts false\` and re-installing.`);
+    console.error(`Otherwise, your platform might not be supported. Open an issue on GitHub!`);
+    process.exit(1);
+}
 
 try {
     execFileSync(
-        path.join(__dirname, `../bin/pagefind_extended${process.platform === 'win32' ? '.exe' : ''}`),
+        binaryPath,
         process.argv.slice(2),
         {
             stdio: [process.stdin, process.stdout, process.stderr]
         }
     )
 } catch (err) {
-    //Purposefully ignore errors as the CLI should handle all the messaging etc.
+    process.exit(1);
 }
