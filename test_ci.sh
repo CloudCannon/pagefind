b501f7d278a63f62bdf75c51e2608305ee034886
Liam Bigelow <40188355+bglw@users.noreply.github.com>
2024-08-17 00:24:36 +1200

    Update to toolproof 0.3.0

--- test_ci.sh
+++ test_ci.sh
@@ -6,4 +6,4 @@ cd $SCRIPT_DIR
 PAGEFIND=$(realpath "$SCRIPT_DIR/target/release/pagefind")
 REPO_WD=$(realpath "$SCRIPT_DIR")
 
-npx -y toolproof --placeholders pagefind_exec_path="$PAGEFIND" repo_wd="$REPO_WD"
+npx -y toolproof@0.3.0 --placeholders pagefind_exec_path="$PAGEFIND" repo_wd="$REPO_WD" -c 4
