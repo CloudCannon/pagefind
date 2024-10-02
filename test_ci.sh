a137669846920355985123e18ba32bb3ef92c42f
Liam Bigelow <40188355+bglw@users.noreply.github.com>
2024-10-02 22:33:38 +1300

    Update Toolproof

--- test_ci.sh
+++ test_ci.sh
@@ -6,4 +6,4 @@ cd "$SCRIPT_DIR"
 PAGEFIND=$(realpath "$SCRIPT_DIR/target/$1/pagefind")
 REPO_WD=$(realpath "$SCRIPT_DIR")
 
-npx -y toolproof@0.4.0 --placeholders pagefind_exec_path="$PAGEFIND" repo_wd="$REPO_WD" -c 1
+npx -y toolproof@0.4.1 --placeholders pagefind_exec_path="$PAGEFIND" repo_wd="$REPO_WD" -c 1
